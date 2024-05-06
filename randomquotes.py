9d1483c245a83c12040848435ab092d060987464
unknown <ezapataa1@eafit.edu.co>
2024-05-06 14:03:15 -0500

    .py modified

--- randomquotes.py
+++ randomquotes.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 import os
 import random
 
@@ -17,9 +17,9 @@ phrases = [
 
 @app.route('/')
 def get_random_quote():
-    phrase = random.choice(phrases)
-    container_id = os.uname()[1] 
-    return f"{phrase} - Container Id: {container_id}"
+    phrase = random.choice(phrases) 
+    container_id = os.uname()[1]  
+    return f"{phrase} - Container Id: {container_id}" 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
